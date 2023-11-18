7265fe3f49e3558bbbc18aacbc1a10f32de96699
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-11-18 21:57:54 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://quotes.toscrape.com'
+
+def parser(base_url):
+    quotes = list()
+    authors = list()
+    count = 0
+
+    while True:
+        count += 1
+        url = f'{base_url}/page/{count}'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        
+        div_quote = soup.find_all('div', class_='quote')
+        if not div_quote:
+            break
+        for el in div_quote:
+            quote = el.find('span', class_='text').text
+            author = el.find('small', class_='author').text
+            author_links = soup.find('a', href=lambda href: href and 'author' in href)['href']
+            all_tags = el.find_all('div', class_='tags')
+            
+
+            for _ in all_tags:
+                tag_links = _.find_all('a')
+                tags = list()
+                for tag_link in tag_links:
+                    tags.append(tag_link.text)
+            print(f"{quote=}\n{author=}\n{author_links=}\n{tags=}\n{BeautifulSoup(requests.get(f'{base_url}/{author_links}').text, 'lxml').find('span', class_='author-born-date').text=}\n{BeautifulSoup(requests.get(f'{base_url}/{author_links}').text, 'lxml').find('span', class_='author-born-location').text}")
+            quotes.append({
+                "tags": tags,
+                "author": author,
+                "quote": quote
+            })
+            authors.append({
+                "fullname": author,
+                "born_date": BeautifulSoup(requests.get(f'{base_url}/{author_links}').text, 'lxml').find('span', class_='author-born-date').text,
+                "born_location": BeautifulSoup(requests.get(f'{base_url}/{author_links}').text, 'lxml').find('span', class_='author-born-location').text,
+                "description": quote
+            })
+        
+    with open('quotes.json', 'w', encoding='utf-8') as fl:
+        json.dump(quotes, fl, indent=2)
+    with open('authors.json', 'w', encoding='utf-8') as fl:
+        json.dump(authors, fl, indent=2)
+
+parser(url)
